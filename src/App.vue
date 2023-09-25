1599e207d200f38e5326d4e8c4f97ffbd92a3c75
Ihor Neizvestnyi <ihor.neizvestnyi@novait.local>
2023-09-25 09:32:53 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="header">
+    <img src="/logo.svg" alt="logo" class="header-logo">
+    <h2>My favourite movies</h2>
+  </header>
+
+</template>
+
+
+<script setup>
+
+</script>
+
+
+
+<style lang="css">
+.header {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+.header-logo {
+  max-width: 50px;
+  margin-right: 10px;
+}
+.btn {
+  border: none;
+  width: 100px;
+  height: 40px;
+  font-size: 14px;
+  margin: 0 10px;
+  border-radius: 10px;
+  cursor: pointer;
+  background: #efefef;
+}
+.btn:hover {
+  opacity: 0.7;
+}
+.btn_green {
+  background: #37df5c;
+}
+
+.tabs {
+  display: flex;
+  justify-content: center;
+  margin-bottom: 30px;
+}
+</style>
