acf7a12283ef7d7bbb35d28505a30ee9d53c4351
joshkrab <neizvestnyi.igor@gmail.com>
2024-01-09 13:44:48 +0200

    feat

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
-  <header class="header">
-    <img src="/logo.svg" alt="logo" class="header-logo" />
-    <h2>My favourite movies</h2>
-  </header>
+  <main>
+    <header class="header">
+      <img src="/logo.svg" alt="logo" class="header-logo" />
+      <h2>My Favorite Movies</h2>
+    </header>
+
+    <div class="tabs">
+      <button :class="['btn', { btn_green: movieStore.activeTab === 1 }]">
+        Favorite
+      </button>
+      <button :class="['btn', { btn_green: movieStore.activeTab === 2 }]">
+        Search
+      </button>
+    </div>
+
+    <div class="movies" v-if="movieStore.activeTab === 1">
+      <h3>All movies</h3>
+      <Movie
+        v-for="movie of movieStore.movies"
+        :key="movie.id"
+        :movie="movie"
+      />
+    </div>
+    <div class="search" v-if="movieStore.activeTab === 2">Search</div>
+  </main>
 </template>
 
-<script setup></script>
+<script setup>
+import { useMovieStore } from "./stores/MovieStore";
+import Movie from "./components/Movie.vue";
+const movieStore = useMovieStore();
+</script>
 
 <style lang="css">
 .header {
